5b125223a884a24c39cd8ee122682456716004ef
eduardaclemente <eduardaclemente.369@gmail.com>
2022-08-17 14:46:50 -0300

    Subindo projeto até parte de exceções

---
+++ src/main/java/br/com/dio/exeception/ProductNullException.java
@@ -0,0 +1,6 @@
+package br.com.dio.exeception;
+
+public class ProductNullException extends NullPointerException {
+
+    private static final long serialVersionUID = 1L;
+}
---
+++ src/main/java/br/com/dio/exeception/ProductPriceException.java
@@ -0,0 +1,6 @@
+package br.com.dio.exeception;
+
+public class ProductPriceException extends Exception {
+
+    private static final long SerialVersionUID = 1L;
+}

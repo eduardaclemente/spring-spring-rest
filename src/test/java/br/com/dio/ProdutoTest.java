a9d8fa7dbfda30339577c5ca85d7a8ce0b99ac41
eduardaclemente <eduardaclemente.369@gmail.com>
2022-08-20 16:47:48 -0300

    inserindo classe teste de ProdutoTest

---
+++ src/test/java/br/com/dio/ProdutoTest.java
@@ -0,0 +1,30 @@
+package br.com.dio;
+
+import br.com.dio.entity.Produto;
+import br.com.dio.exeception.ProductPriceException;
+import br.com.dio.service.ProdutoService;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+@SpringBootTest
+public class ProdutoTest {
+
+    @Autowired
+    private ProdutoService produtoService;
+
+    @Test
+    public void verificaValorNegativoNoProduto() throws Exception {
+        Produto produto = new Produto();
+
+        produto.setNome("Teste");
+        produto.setPreco(-10.0);
+
+        assertThrows(ProductPriceException.class, () -> produtoService.save(produto));
+
+
+    }
+
+}
